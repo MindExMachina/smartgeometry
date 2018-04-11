2d5806e488e64f207c682a5ca981591008580be2
Nono Martínez Alonso <mundowarezweb@gmail.com>
2018-04-11 08:11:53 -0400

    Add first version of teachable-machina with WebSockets client.

---
+++ teachable-machina/README.md
@@ -0,0 +1,42 @@
+# Teachable Machine 
+## About
+Teachable Machine is an experiment that makes it easier for anyone to explore machine learning, live in the browser – no coding required. Learn more about the experiment and try it yourself on [g.co/teachablemachine](https://g.co/teachablemachine). 
+
+The experiment is built using the [deeplearn.js](https://github.com/PAIR-code/deeplearnjs) library.
+
+We have also released a boilerplate version of this project that can be used as a starting point for your own projects: [googlecreativelab/teachable-machine-boilerplate](https://github.com/googlecreativelab/teachable-machine-boilerplate)
+
+## Development
+#### Install dependencies by running (similar to `npm install`)
+```
+yarn
+```
+
+#### Build project
+```
+yarn build
+```
+
+#### Start local server by running 
+```
+yarn run watch
+```
+
+#### Code Styles
+- There’s a pre-commit hook set up that will prevent commits when there are errors
+- Run `yarn eslint` for es6 errors & warnings
+- Run `yarn stylint` for stylus errors & warnings
+
+#### To run https locally:
+https is required to get camera permissions to work when not working with `localhost`
+
+1. Generate Keys
+```
+openssl genrsa -out server.key 2048
+openssl req -new -x509 -sha256 -key server.key -out server.cer -days 365 -subj /CN=YOUR_IP
+```
+2. Use `yarn run watch-https`
+3. Go to `https://YOUR_IP:3000`, then accept the insecure privacy notice, and proceed.
+
+## Credit
+This is not an official Google product, but an experiment that was a collaborative effort by friends from [Støj](http://stoj.io/), [Use All Five](https://useallfive.com/) and Creative Lab and [PAIR](https://ai.google/pair/) teams at Google.
---
+++ teachable-machina/src/ui/modules/WiresRight.js
@@ -0,0 +1,254 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+class WiresRight {
+    constructor(element) {
+        this.element = element;
+
+        this.canvas = document.createElement('canvas');
+        this.bulbVert = true;
+        this.bulbSmall = false;
+
+        this.bulbGreen = this.element.querySelector('.wire--right-bulb-green-glow');
+        this.bulbPurple = this.element.querySelector('.wire--right-bulb-purple-glow');
+        this.bulbOrange = this.element.querySelector('.wire--right-bulb-orange-glow');
+
+        this.element.appendChild(this.canvas);
+        this.context = this.canvas.getContext('2d');
+
+        this.offsetY = 0;
+        this.animator = {};
+
+        for (let index = 0; index < 3; index += 1) {
+
+            let bulbElement = document.createElement('div');
+
+            bulbElement.classList.add('wires__bulb');
+            bulbElement.classList.add('wires__bulb-' + GLOBALS.classNames[index]);
+            this.size();
+
+            this.element.appendChild(bulbElement);
+
+            this.animator[index] = {
+                highlight: false,
+                bulb: bulbElement
+            };
+        }
+
+        window.addEventListener('resize', () => {
+            if (window.innerWidth <= 900) {
+                this.canvas.style.display = 'none';
+            }else {
+                this.canvas.style.display = 'block';
+            }
+        });
+
+        window.addEventListener('orientationchange', () => {
+            this.size();
+        });
+
+        this.altOffset = 340;
+        this.size();
+        this.loops = 10;
+        this.current = 0;
+        this.running = false;
+        this.renderOnce = true;
+        this.bulbVert ? this.render() : this.renderAlt();
+
+        if (this.bulbVert) {
+            this.offsetY = -90;
+            this.startY = 190 + this.offsetY;
+
+            this.renderOnce = true;
+            this.render();
+        }else {
+            this.altOffset = 300;
+            this.size();
+            this.renderAlt();
+        }
+    }
+
+    render(once) {
+        this.context.clearRect(0, 0, this.width, this.height);
+        this.context.lineWidth = 3;
+
+        for (let index = 0; index < 3; index += 1) {
+            let startY = this.startY + (this.startSpace * index);
+
+            let start = {
+                x: this.startX,
+                y: this.startY + (this.startSpace * index)
+            };
+
+            let end = {
+                x: this.endX,
+                y: this.endY + (this.endSpace * index)
+            };
+
+            let cp1 = {
+                x: 5,
+                y: start.y
+            };
+
+            let cp2 = {
+                x: 25,
+                y: end.y
+            };
+            this.context.strokeStyle = '#cfd1d2';
+            this.context.beginPath();
+            this.context.moveTo(start.x, start.y);
+            this.context.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, end.x, end.y);
+            this.context.stroke();
+        }
+
+        if (this.renderOnce) {
+            this.renderOnce = false;
+        }else {
+            if (this.current < this.loops) {
+                this.current += 1;
+            }else {
+                this.dehighlight();
+            }
+            this.running = true;
+            this.timer = requestAnimationFrame(this.render.bind(this));
+        }
+    }
+
+    renderAlt(once) {
+
+        this.context.clearRect(0, 0, this.width, this.height);
+        this.context.lineWidth = 3;
+
+        for (let index = 0; index < 4; index += 1) {
+            let startY = this.startY + (this.startSpace * index);
+
+            let start = {
+                x: this.startX,
+                y: this.startY + (this.startSpace * index)
+            };
+
+            let end = {
+                x: this.endX,
+                y: this.endY + (this.endSpace * index)
+            };
+
+
+            this.context.strokeStyle = '#cfd1d2';
+            this.context.beginPath();
+            this.context.moveTo(start.x, start.y);
+            this.context.lineTo(end.x, start.y);
+            this.context.stroke();
+        }
+
+        if (this.renderOnce) {
+            this.renderOnce = false;
+        }else {
+            if (this.current < this.loops) {
+                this.current += 1;
+            }else {
+                this.dehighlight();
+            }
+            this.timer = requestAnimationFrame(this.renderAlt.bind(this));
+        }
+    }
+
+    highlight(id) {
+        let index = GLOBALS.classNames.indexOf(id);
+        // console.log(index);
+        // if (this.animator[index]) {
+        //     let animator = this.animator[index];
+        //     animator.bulb.classList.add('wires__bulb--selected');
+        //     this.highlightedLineIndex = index;
+        //     this.current = 0;
+        //     this.start();
+        // }
+        switch (index) {
+            case 0:
+            this.bulbGreen.classList.add('bulb--selected');
+            break;
+            case 1:
+            this.bulbOrange.classList.add('bulb--selected');
+            break;
+            case 2:
+            this.bulbPurple.classList.add('bulb--selected');
+            break;
+            default:
+        }
+    }
+
+    dehighlight() {
+        this.bulbGreen.classList.remove('bulb--selected');
+        this.bulbPurple.classList.remove('bulb--selected');
+        this.bulbOrange.classList.remove('bulb--selected');
+    }
+
+    start() {
+        if (!this.running) {
+            this.timer = requestAnimationFrame(this.bulbVert ? this.render.bind(this) : this.renderAlt.bind(this));
+        }
+    }
+
+    stop() {
+        cancelAnimationFrame(this.timer);
+        this.running = false;
+    }
+
+    size() {
+        const BREAKPOINT_DESKTOP = 900;
+        const BREAKPOINT_MED = 428;
+
+        this.width = this.element.offsetWidth;
+        let bulbs = Array.from(document.getElementsByClassName('wires__bulb'));
+        this.startSpace = (this.height - 80) / 3;
+        // console.log(this.startSpace);
+        this.startSpace = 130;
+        this.endSpace = (this.height + 45) / 5;
+        this.canvas.width = 70;
+        this.canvas.height = this.height;
+
+        // this element rotated in css and using height as width
+        // if (window.innerWidth >= BREAKPOINT_DESKTOP) {
+        this.height = 450;
+
+        // remove offset on desktop
+        this.element.setAttribute('style', '');
+
+        bulbs.forEach((bulb, index) => {
+            bulb.style.top = this.endY + (index * this.endSpace) + 'px';
+        });
+
+        this.bulbVert = true;
+
+        this.startX = 0;
+        this.startY = 190 + this.offsetY;
+
+        // this.endSpace = this.height / 5;
+        this.endX = this.width;
+        // this.endY = (this.height / 2) - (2 * this.endSpace) + 25;
+        this.endY = 145;
+
+        if (window.innerWidth <= BREAKPOINT_DESKTOP) {
+            this.canvas.style.display = 'none';
+
+            bulbs.forEach((bulb, index) => {
+                bulb.style.top = 'none';
+            });
+        }
+    }
+
+}
+
+import GLOBALS from './../../config.js';
+
+export default WiresRight;
