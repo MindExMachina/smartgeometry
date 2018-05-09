5fb55ea319aeba87f6b545a0632c1aa84d48caa1
Nono Martínez Alonso <mundowarezweb@gmail.com>
2018-05-08 21:20:03 -0400

    Add simple_predict_absolute GET route.

--- services/sketch-rnn/http-server.js
+++ services/sketch-rnn/http-server.js
@@ -121,6 +121,45 @@ app.post('/simple_predict_absolute', function(req, res) {
 
 });
 
+
+
+/**
+ * A GET route to request a prediction from SketchRNN
+ * (from an absolute set of strokes).
+ */
+app.get('/simple_predict_absolute', function(req, res) {
+
+    // Load simple_predict.js module
+    //var simple_predict = require('./lib/simple_predict');
+
+    // Get strokes from the POST request's parameters
+    var strokes = req.param('strokes');
+    var model = req.param('model');
+    if (model) {
+        console.log("Requesting model " + model);
+        simple_predict.load_model(model);
+    } else {
+        console.log("No model requested");
+    }
+
+    // If strokes are provided on the request,
+    // set them as input strokes for simple_predict
+    if (strokes) {
+        var strokes = JSON.parse(strokes);
+        simple_predict.set_absolute_strokes(strokes)
+    }
+
+    // Request a sketch prediction
+    simple_predict.predict();
+
+    // Get the predicted strokes
+    var predicted_strokes = simple_predict.output_strokes_absolute();
+
+    // Return the predicted strokes in the response
+    res.json(predicted_strokes);
+
+});
+
 /**
  * A GET route to request a prediction from SketchRNN.
  * 
